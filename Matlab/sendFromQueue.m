b70e12040f42ea2156116b830bdf1f86877ec461
GuineaBrown <42484589+GuineaBrown@users.noreply.github.com>
2018-09-19 00:58:37 +1000

    Update sendFromQueue.m

--- Matlab/sendFromQueue.m
+++ Matlab/sendFromQueue.m
@@ -1,5 +1,9 @@
-function sendFromQueue(queue, socket)
+function queue = sendFromQueue(queue, socket)
+  %takes the first command on the queue and sends
 
-
-
+  cmdToSend = queue(1)
+  queue(1) = [];
+  
+  fwrite(socket, cmdToSend);
+  
 end
