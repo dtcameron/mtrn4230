38bb009970fd62d4212eda6d3b96723157b117aa
GuineaBrown <42484589+GuineaBrown@users.noreply.github.com>
2018-09-19 02:12:49 +1000

    Update and rename sendFromQueue.m to sendToRobot.m

--- Matlab/sendToRobot.m
+++ Matlab/sendToRobot.m
@@ -1,5 +1,9 @@
-function queue = sendFromQueue(queue, socket)
+function queue = sendToRobot(queue, socket)
   %takes the first command on the queue and sends
+  
+  if isempty(queue)
+    queue(1) = 'FC' %status check by default if empty)
+  end
 
   cmdToSend = queue(1)
   queue(1) = [];
