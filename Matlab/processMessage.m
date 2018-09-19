dcab635b36701899d59a37d61bac686e86322624
GuineaBrown <42484589+GuineaBrown@users.noreply.github.com>
2018-09-19 20:15:54 +1000

    Add files via upload

--- Matlab/processMessage.m
+++ Matlab/processMessage.m
@@ -4,20 +4,20 @@ function [status, safety, DIO, joints] = processMessage(x)
 
 
     status.Head = x(1);
-    status.Status  = x(2:5);
+    status.Status  = [x(2) x(3) x(4) x(5)];
     status.PrevCmd = x(6);
     
             % Estop LC  Mot   HtE   MTE
-    safety = [x(7) x(8) x(9) x(10) x(11)];
+    safety = [x(7) x(8) x(9) x(10) x(11)]; %doesn't need additional processing
     
-    DIO = [x(12) x(13) x(14) x(15) x(16)];
+    DIO = [x(12) x(13) x(14) x(15) x(16)]; %also doesn't need additional processing
     
-    joints = [x(17:18);       ... % J1
-              x(19:20);       ... % J2
-                x(21:22);     ... % J3
-    x(23:24);                 ... % J4
-    x(25:26);                 ... % J5
-    x(27:28);];                   % J6
+    joints = [x(17:18);     ... % J1
+              x(19:20);     ... % J2
+              x(21:22);     ... % J3
+              x(23:24);     ... % J4
+              x(25:26);     ... % J5
+              x(27:28);];       % J6
 
     
 
