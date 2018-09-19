fff1398f1195b9802670276136e7d5edd57aaa6d
Dennis <865807250@qq.com>
2018-09-19 13:17:49 +1000

    Add files via upload

---
+++ Matlab/SafetySystem.m
@@ -0,0 +1,21 @@
+function SafetySystem(app, safety)
+
+if any(safety)
+    app.ERRORLamp.Color = 'r';
+    if safety(1) == 1
+        app.ErrorStateText.Value = 'Emergency stop activated';
+    elseif safety(2) == 1
+        app.ErrorStateText.Value = 'Light curtain activated';
+    elseif safety(3) == 1
+        app.ErrorStateText.Value = 'Motors off';
+    elseif safety(4) == 1
+        app.ErrorStateText.Value = 'Hold to enable not pressed';
+    elseif safety(5) == 1
+        app.ErrorStateText.Value = 'Motion task not running/Execution Error';
+    end
+else
+    app.ERRORLamp.Color = 'g';
+    app.ErrorStateText.Value = '';
+end
+
+end
